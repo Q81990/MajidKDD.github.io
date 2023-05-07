7d89f317547fdbb4ce94eb381876c9f51d5d00ed
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-05-07 14:24:50 +1000

    Create imgfile.js

---
+++ imgfile.js
@@ -0,0 +1,37 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// define the component
+export class ImageControl extends LitElement {
+  
+  static properties = {
+    imageSrc: {type: String},
+  };
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Image Control',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        imageSrc: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        }
+      }
+    };
+  }
+  
+  constructor() {
+    super();
+    this.imageSrc = '';
+  }
+
+  render() {
+    return html`<img src="${this.imageSrc}" alt="Preview Image">`;
+  }
+}
+
+// registering the web component
+const elementName = 'myimage-control';
+customElements.define(elementName, ImageControl);
