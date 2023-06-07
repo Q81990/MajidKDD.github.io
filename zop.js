92c8fee041a6c4d64c8a4bb31e3d31b0ce0c70b7
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-06-07 21:02:52 +1000

    Create zop.js

---
+++ zop.js
@@ -0,0 +1,44 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+export class WebIndex extends LitElement {
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.fetchData();
+
+    }
+
+    fetchData() {
+        fetch('https://api.zippopotam.us/us/90210')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                };
+                response.json();
+            })
+            .then(data => {
+                this.data = data;
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+  
+constructor() {
+    super();
+      }
+
+    render() {
+        if (!this.data) {
+            return html`
+                <h4>Loading...</h4>
+            `;
+        }
+        return html`
+            <h4>Done</h4>
+        `;
+    }
+
+}
+
+customElements.define('web-index', WebIndex);
