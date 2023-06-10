9cb069de88ccd4a4d6be7a006d409b1b4a419e25
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-06-10 20:02:57 +1000

    Update zop.js

--- zop.js
+++ zop.js
@@ -1,44 +1,79 @@
-import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-export class WebIndex extends LitElement {
+import {css, html, LitElement, styleMap, until} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-    connectedCallback() {
-        super.connectedCallback();
-        this.fetchData();
-
-    }
-
-    fetchData() {
-        fetch('https://api.zippopotam.us/us/90210')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                };
-                response.json();
-            })
-            .then(data => {
-                this.data = data;
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
-  
-constructor() {
-    super();
+export class EmbeddedAssureSign extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
+    static styles = css`
+      :host {
+        height: 100%;
+        width: 100%;
+        display: block;
       }
 
-    render() {
-        if (!this.data) {
-            return html`
-                <h4>Loading...</h4>
-            `;
-        }
-        return html`
-            <h4>Done</h4>
-        `;
+      .frame {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        background-color: transparent;
+        border: none;
+      }
+    `;
+    
+    static properties = {
+        content: { type : String }      
+    }
+    
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+            controlName: 'UPN',
+            fallbackDisableSubmit: false,
+            description: 'upn',
+            iconUrl: "pen",
+            groupName: 'AUS UPN',
+            version: '1.3',
+            properties: {
+                height: {
+                    type: 'string',
+                    title: 'Height',
+                    description: 'Height of the component',
+                }               
+            },
+            standardProperties: {
+                readOnly: true,
+                description: true,
+            }
+        };
+    }
+    
+    async load() {
+
+     
+        const signingLinks = await fetch('https://api.zippopotam.us/us/90210');    
+        const jsonSigningLinks = await signingLinks.json();
+        
+        let styles = {height: this.height};
+        return    return html`
+        <p>${jsonSigningLinks.country} </p>  
+      `;   
+            
+    }
+    
+    constructor() {
+        super();        
+        this.height = '900px'
     }
 
+    async connectedCallback() {
+        super.connectedCallback();
+        this.content = this.load();
+    }
+
+    // Render the UI as a function of component state
+    render() {
+        return html`${until(this.content, html`<span>Loading...</span>`)}`
+    }
 }
 
-customElements.define('web-index', WebIndex);
+// registering the web component.
+const elementName = 'zip-zap';
+customElements.define(elementName, EmbeddedAssureSign);
