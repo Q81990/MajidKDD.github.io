8a83e621135786fbaacf06c4aa623973145e7114
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-10 16:58:26 +1000

    Update abns.js

--- abns.js
+++ abns.js
@@ -1,21 +1,21 @@
 import { css, html, LitElement, styleMap, until } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-export class abnControlz extends LitElement {
+class ABNControlz extends LitElement {
 
   static properties = {
     videoUrl: { type: String },
-    identifierValues: { type: Array },
+    tableData: { type: Array },  // New property to store table data
   };
 
   static getMetaConfig() {
     return {
-      controlName: 'abn Control',
+      controlName: 'ABN Control',
       fallbackDisableSubmit: false,
       version: '1.2',
       properties: {
         videoUrl: {
           type: 'string',
-          title: 'abnURL',
+          title: 'ABN URL',
           description: 'Type the YouTube URL here'
         }
       }
@@ -28,36 +28,61 @@ export class abnControlz extends LitElement {
     const myJson = await response.text();
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(myJson, 'text/xml');
-    const identifierNodes = xmlDoc.getElementsByTagName("identifierValue");
-    const identifierValues = [];
-    for (const node of identifierNodes) {
-      identifierValues.push(node.childNodes[0].nodeValue);
-    }
-    return identifierValues;
+    
+    const searchResults = xmlDoc.querySelectorAll('searchResultsRecord');
+    const tableData = Array.from(searchResults).map(result => {
+      const abn = result.querySelector('identifierValue').textContent;
+      const name = result.querySelector('organisationName').textContent;
+      const type = result.querySelector('identifierStatus').textContent;
+      const stateCode = result.querySelector('stateCode').textContent;
+      const postcode = result.querySelector('postcode').textContent;
+      const location = stateCode ? `${stateCode}, ${postcode}` : '';
+      return { abn, name, type, location };
+    });
+    
+    return tableData;
   }
 
   async connectedCallback() {
     super.connectedCallback();
-    this.identifierValues = await this.load();
+    this.tableData = await this.load();
   }
 
-  renderDropdown() {
-    const options = this.identifierValues.map(value => html`<option value="${value}">${value}</option>`);
+  renderTable() {
+    const rows = this.tableData.map(row => html`
+      <tr>
+        <td>${row.abn}</td>
+        <td>${row.name}</td>
+        <td>${row.type}</td>
+        <td>${row.location}</td>
+      </tr>
+    `);
+
     return html`
-      <select>
-        <option value="">Select Identifier</option>
-        ${options}
-      </select>
+      <table>
+        <thead>
+          <tr>
+            <th>ABN</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${rows}
+        </tbody>
+      </table>
     `;
   }
 
   render() {
     return html`
-      <p>ABN Data:</p>
-      ${this.renderDropdown()}
+      <div>
+        <h2>ABN Data:</h2>
+        ${this.renderTable()}
+      </div>
     `;
   }
 }
 
-const elementName = 'abn-controlsz';
-customElements.define(elementName, abnControlz);
+customElements.define('abn-controlzz', ABNControlz);
