38b8d06976fed7cf96876903d709fc69506cc424
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-19 13:04:28 +1000

    Update Snow.js

--- Snow.js
+++ Snow.js
@@ -29,32 +29,19 @@ export class snowControl extends LitElement {
   }
   
   
-  async load() {
- //   const abnvar = 'https://abr.business.gov.au/ABRXMLSearch/AbrXmlSearch.asmx/ABRSearchByABN?searchString='+this.videoUrl+'&includeHistoricalDetails=N&authenticationGuid=a1aceb80-e8bd-46f0-a5e1-e232c4a4c417';
- //const abnvar =   'https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/ABRSearchByNameAdvancedSimpleProtocol2017?name='+this.videoUrl+'&legalName=&tradingName=&businessName=&activeABNsOnly=&NSW=&SA=&ACT=&postcode=&VIC=&WA=&NT=&QLD=&TAS=&authenticationGuid=a1aceb80-e8bd-46f0-a5e1-e232c4a4c417&searchWidth=&minimumScore=&maxSearchResults=';
- // const response = await fetch(abnvar);
-
-        const response = await fetch('https://dev160993.service-now.com/api/now/table/incident?sysparm_fields=number&caller_id=majid',
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Basic YWRtaW46dmJKYWRASCpUNlc5',  
-                'Content-Type': 'application/json',
-            }
-        });
-
-    const myJson = await response.text();
+async load() {
+const snowvar =   'https://dev160993.service-now.com/api/now/table/incident?sysparm_fields=number&caller_id=majid';
+  const response = await fetch(snowvar,{ method: "GET", headers: { "Authorization": "Basic YWRtaW46dmJKYWRASCpUNlc5"}});
+if (response.ok) {
+      const myJson = await response.text();
     const countryPromise = Promise.resolve(myJson);
-  
-       // const parser = new DOMParser();
-       // const xmlDoc = parser.parseFromString(myJson, 'text/xml');
-        //const xpathResult = xmlDoc.getElementsByTagName("organisationName")[0].childNodes[0].nodeValue+'|   '+xmlDoc.getElementsByTagName("effectiveFrom")[0].childNodes[0].nodeValue+'|   '+xmlDoc.getElementsByTagName("ASICNumber")[0].childNodes[0].nodeValue;
-        //  const xpathResult = xmlDoc.getElementsByTagName("organisationName")[0].childNodes[0].nodeValue+'|   '+xmlDoc.getElementsByTagName("score")[0].childNodes[0].nodeValue+'|   '+xmlDoc.getElementsByTagName("identifierValue")[0].childNodes[0].nodeValue;
-    const country = await myJson;    //xpathResult; 
+     const country = await countryPromise; 
     return country;
-    //html `  <div>Country: ${country}</div>  `;  
-  }
+      } else {
+        return "Error";
+    }
   
+  }  
  
   constructor() {
     super();
