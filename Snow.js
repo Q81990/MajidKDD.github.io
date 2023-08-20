f174d441f2488f56d2aaaa703cd2c06e2ce51679
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-20 13:21:14 +1000

    Update Snow.js

--- Snow.js
+++ Snow.js
@@ -1,24 +1,52 @@
 import {css, html, LitElement, styleMap, until} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 // define the component
-export class abnControl extends LitElement {
+export class snowControl extends LitElement {
+  
+  static styles = css`
+    .selcls {
+      padding: 9px;
+      border: solid 5px #c9302c;
+      outline: 0;
+      background: -webkit-gradient(
+        linear,
+        left top,
+        left 25,
+        from(#ffffff),
+        color-stop(4%, #e78f8d),
+        to(#ffffff)
+      );
+      background: -moz-linear-gradient(
+        top,
+        #ffffff,
+        #e78f8d 1px,
+        #ffffff 25px
+      );
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+      -moz-box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+      -webkit-box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+      border-radius: 13px;
+      width: 150px;
+    }
+  `;
+  
   
   static properties = {
-    videoUrl: {type: String},
-selectedOption: { type: String }
+    incnum: {type: String},
+    selectedOption: { type: String }
   };
   
   // return a promise for contract changes.
   static getMetaConfig() {
     return {
-      controlName: 'abn Control',
+      controlName: 'SNow Control',
       fallbackDisableSubmit: false,
       version: '1.2',
       properties: {
-        videoUrl: {
+        incnum: {
           type: 'string',
-          title: 'abnURL',
+          title: 'IncidentNumber',
           // isValueField: true,
-          description: 'Type the YouTube URL here'
+          description: 'Type the Inncident number here'
         }
       }
     };
@@ -26,32 +54,23 @@ selectedOption: { type: String }
   
   
   renderCountry(country) {
-    this.videoUrl =  html `  <div>Country: ${country}</div>  `;
+    this.incnum =  html `  <div>Country: ${country}</div>  `;
   }
   
   
-  async load() {
-const apiUrl = 'https://dev160993.service-now.com/api/now/table/incident?sysparm_fields=number&caller_id=majid';
-const headers=
-    {"Content-Type": "application/json",      
-      "Authorization": "Basic YWRtaW46dmJKYWRASCpUNlc5"}
-      
-    
-
-
-const abnvar =   'https://dev160993.service-now.com/api/now/table/incident?sysparm_fields=number&caller_id=majid';
- const response = await fetch(abnvar,{ method: "GET", headers: { "Authorization": "Basic YWRtaW46dmJKYWRASCpUNlc5"}});
+async load() {
+const snowvar =   'https://dev160993.service-now.com/api/now/table/incident?sysparm_fields=number&caller_id=majid';
+  const response = await fetch(snowvar,{ method: "GET", headers: { "Authorization": "Basic YWRtaW46dmJKYWRASCpUNlc5"}});
 if (response.ok) {
-        
-          const myJson = await response.json();
+         const myJson = await response.json();
       const numbers = myJson.result.map(item => item.number);
 
       return numbers;
-    } else {
-        return "hellhhNOeeb";
+      } else {
+        return "Error";
     }
-  }
   
+  }  
  
   constructor() {
     super();
@@ -60,23 +79,24 @@ if (response.ok) {
 
       async connectedCallback() {
         super.connectedCallback();
-         this.videoUrl = await this.load();
+         this.incnum = await this.load();
         //await this.load();
     }
+  
+  render() {
+    const dropdownOptions = this.incnum.map(
+      (number) => html`<option class="selcls" value="${number}">${number}</option>`
+    );
 
- render() {
- const dropdownOptions = this.videoUrl.map(number => html`<option value="${number}">${number}</option>`);
-
-  return html`
-    <label for="numberDropdown">Select a Number:</label>
-    <select id="numberDropdown" @change="${this.handleDropdownChange}">
-      ${dropdownOptions}
-    </select>
-    <label for="selectedValue">Selected Value:</label>
-    <input id="selectedValue" type="text" .value="${this.selectedOption}" readonly>
-  `;
-     
-}
+    return html`
+      <label for="numberDropdown">Select a Number:</label>
+      <select id="numberDropdown" @change="${this.handleDropdownChange}">
+        ${dropdownOptions}
+      </select>
+      <label for="selectedValue">Selected Value:</label>
+      <input id="selectedValue" type="text" .value="${this.selectedOption}" readonly />
+    `;
+  }
   
 handleDropdownChange(event) {
   const selectedValue = event.target.value;
@@ -84,7 +104,6 @@ handleDropdownChange(event) {
 }
 
 }
-
 // registering the web component
-const elementName = 'abn-control';
-customElements.define(elementName, abnControl);
+const elementName = 'snow-control';
+customElements.define(elementName, snowControl);
