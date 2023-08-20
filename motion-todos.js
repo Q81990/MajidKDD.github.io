c1796b80d3abdc2333020fec842950b4292cd943
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-20 15:47:00 +1000

    Update motion-todos.js

--- motion-todos.js
+++ motion-todos.js
@@ -2,7 +2,6 @@ import {LitElement, html} from 'lit';
 import {classMap} from 'lit/directives/class-map.js';
 import {repeat} from 'lit/directives/repeat.js';
 import {animate, fadeOut, flyBelow} from '@lit-labs/motion';
-import {styles} from 'https://majidkdd.github.io/styles.js';
 import '@material/mwc-textfield';
 import '@material/mwc-button';
 import '@material/mwc-checkbox';
@@ -19,6 +18,73 @@ const data = [
 ];
 
 export class MotionTodos extends LitElement {
+ static styles = css`
+    :host {
+      display: inline-block;
+      outline: none;
+      padding: 8px;
+      --mdc-theme-primary: #0069c0;
+      --mdc-theme-secondary: #1b5e20;
+      --mdc-typography-body2-font-size: 1.1rem;
+      --mdc-typography-body2-font-weight: 600;
+      --mdc-checkbox-unchecked-color: black;
+    }
+
+    mwc-textfield {
+      display: block;
+      margin-top: 16px;
+      --mdc-shape-small: 12px;
+    }
+
+    .controls {
+      display: flex;
+      padding: 8px 0;
+      justify-content: flex-end;
+    }
+
+    .lists {
+      display: flex;
+    }
+
+    .list {
+      flex: 1;
+    }
+
+    ul {
+      margin: 0;
+      padding: 0;
+      outline: none;
+    }
+
+    li {
+      will-change: transform;
+      position: relative;
+      background: #ffeb3b;
+      padding: 8px;
+      border-radius: 12px;
+      margin: 8px;
+      display: flex;
+      align-items: center;
+    }
+
+    li > button {
+      border: none;
+      background: none;
+      outline: none;
+      font-family: 'Material Icons';
+      font-size: 24px;
+      cursor: pointer;
+    }
+
+    li > mwc-formfield {
+      flex: 1;
+    }
+
+    .list.completed li {
+      background: #4caf50;
+    }
+    `;
+  
   static properties = {
     data: {type: Array},
   };
