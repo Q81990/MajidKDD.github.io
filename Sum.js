b788966a31d14821f46bbc233db9037902b929ed
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-21 12:28:05 +1000

    Update Sum.js

--- Sum.js
+++ Sum.js
@@ -4,50 +4,8 @@ export class StarsPlugin extends LitElement {
   static properties = {
     num1: { type: Number },
     num2: { type: Number },
-    outcome: { type: Number },
   };
-  static styles = css`
-        *{
-            margin: 0;
-            padding: 0;
-        }
-        .rate {
-            float: left;
-            height: 46px;
-            padding: 0 10px;
-        }
-        .rate:not(:checked) > input {
-            position:absolute;
-            top:-9999px;
-        }
-        .rate:not(:checked) > label {
-            float:right;
-            width:1em;
-            overflow:hidden;
-            white-space:nowrap;
-            cursor:pointer;
-            font-size:30px;
-            color:#ccc;
-        }
-        .rate:not(:checked) > label:before {
-            content: 'â˜… ';
-        }
-        .rate > input:checked ~ label {
-            color: #ffc700;    
-        }
-        .rate:not(:checked) > label:hover,
-        .rate:not(:checked) > label:hover ~ label {
-            color: #deb217;  
-        }
-        .rate > input:checked + label:hover,
-        .rate > input:checked + label:hover ~ label,
-        .rate > input:checked ~ label:hover,
-        .rate > input:checked ~ label:hover ~ label,
-        .rate > label:hover ~ input:checked ~ label {
-            color: #c59b08;
-        }
-        /* Modified from: https://github.com/mukulkant/Star-rating-using-pure-css */        
-  `;
+
   static getMetaConfig() {
     return {
       controlName: 'Number Addition',
@@ -67,32 +25,19 @@ export class StarsPlugin extends LitElement {
           description: 'Enter the second number',
           isValueField: true,
         },
-        outcome: {
-          title: 'Sum',
-          type: 'integer',
-          description: 'Sum of Number 1 and Number 2',
-          isValueField: true,
-        },
       },
-      events: ['ntx-value-change'],
+      events: [],
     };
   }
 
-  _handleInputChange() {
-    const num1 = this.num1 || 0;
-    const num2 = this.num2 || 0;
-    const sum = num1 + num2;
-
-    this.outcome = sum;
-  }
-
   render() {
+    const sum = (this.num1 || 0) + (this.num2 || 0);
+
     return html`
       <input
         type="number"
         placeholder="Calculated Sum"
-        .value=${this.outcome}
-        @input=${this._handleInputChange}
+        .value=${sum}
       />
     `;
   }
