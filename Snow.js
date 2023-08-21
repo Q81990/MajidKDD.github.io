414b869d528f958b450b4153321e27cd4e53e03b
MajidKDD <132728308+MajidKDD@users.noreply.github.com>
2023-08-21 16:01:16 +1000

    Update Snow.js

--- Snow.js
+++ Snow.js
@@ -100,26 +100,13 @@ export class snowControl extends LitElement {
     `;
   }
 
-handleDropdownChange(event) {
-  const selectedOptionElement = event.target.selectedOptions[0]; // Get the selected option element
-  const selectedNumber1 = selectedOptionElement.value; // Get the value (incident number) of the selected option
-  const selectedtext = selectedOptionElement.textContent
 
-  this.selectedOption = selectedNumber1; // Update the selectedOption property
-  const selectedValueInput = this.shadowRoot.querySelector("#selectedValue");
-  selectedValueInput.style.width = (selectedNumber1.length + 1) + "ch";
-
-  const args = {
-    bubbles: true,
-    cancelable: false,
-    composed: true,
-    detail: selectedtext, // Pass the selected incident number as the detail
-  };
-
-  const eventz = new CustomEvent('ntx-value-change', args);
-  this.dispatchEvent(eventz);
-}
-
+  handleDropdownChange(event) {
+    const selectedValue = event.target.value;
+    this.selectedOption = selectedValue;
+    const selectedValueInput = this.shadowRoot.querySelector("#selectedValue");
+    selectedValueInput.style.width = (selectedValue.length + 1) + "ch";
+  }
 
  
   handleInput(event) {
